1c5ffd52ba8df7d0c1d119d82765a60e0ca52da1
aykutyrdm <aykutyrdm@gmail.com>
2019-12-17 20:03:32 +0300

    version_1.9.1

--- api/dockers/controllers/Dockers.js
+++ api/dockers/controllers/Dockers.js
@@ -1,33 +1,27 @@
 'use strict';
 
-/**
- * Read the documentation () to implement custom controller functions
- */
-
 module.exports = {
 
     withBikes : async ctx => {
 
-        try{
-            const dockers = await strapi.query('dockers').find();
-            const dockerNumber = dockers.length;
-            var availResponse = [];
-            for(var i = 0 ; i< dockerNumber; i++){
-                var currentDocker = dockers[i];
-                const currentAvails = await strapi.query('bikes').find({lastDockerId: currentDocker.id, isAvailable : true});
-                availResponse.push ({
-                    currentDocker,
-                    availableBikeNumber: currentAvails.length
-                });
-            } 
-            //Return Response
-            const status = 200;
-            const message = 'OK';
-            return ctx.response.send({ status : status, data : availResponse, message : message });
-        
-        }catch(err){
-            ctx.send({message: err});
+        //Find Dockers and it's bikes that are available currently
+        const dockers = await strapi.query('dockers').find();
+        const dockerNumber = dockers.length;
+        var dockersWithBikes = [];
+        for(var i = 0 ; i< dockerNumber; i++){
+            var currentDocker = dockers[i];
+            const currentAvails = await strapi.query('bikes').find({lastDockerId : currentDocker.id, isAvailable : true});
+            dockersWithBikes.push ({
+                currentDocker,
+                availableBikeNumber: currentAvails.length
+            });
         }
-
+        //Return Response
+        const res = {
+            status : 200,
+            data : dockersWithBikes,
+            message : 'OK'
+        };  
+        return ctx.send(res);
     },
 };
--- api/usages/controllers/Usages.js
+++ api/usages/controllers/Usages.js
@@ -1,147 +1,204 @@
 'use strict';
 
-/**
- * Read the documentation () to implement custom controller functions
- */
 
 module.exports = {
 
     startSession: async ctx => {
-        //Check user has no bike currently
-        const currentUsage = await strapi.query('usages').findOne({userId : ctx.request.body.userId , isOpen : true});
-        if(currentUsage){
-            const status = 400;
-            const error = "Start Session Error!";
-            const message = 'Bike ' + currentUsage.bikeId + ' is already in use by you!';
-            return ctx.response.send({ status : status, error : error, message : message });
-        } 
-        
+
         //Check the bike QR trying to tag
-        const bike = await strapi.query('bikes').findOne({barcode : ctx.request.body.qrCode});
-        if(!bike){
-            const status = 404;
-            const error = "Start Session Error!";
-            const message = 'Invalid QR Code!';
-            return ctx.response.send({ status : status, error : error, message : message });
+        const bike = await Bikes.findOne({barcode : ctx.request.body.qrCode});
+        if(!bike){  
+            const res = {
+                status : 404,
+                errorCode : -101,
+                message : 'Invalid QR Code!'
+            }; 
+            return ctx.send(res);
         }
 
         //Check the bike availability
         if(!bike.isAvailable){
-            const status = 400;
-            const error = "Start Session Error";
-            const message = 'Bike ' + bike.id + ' is already in use!';
-            return ctx.response.send({ status : status, error : error, message : message });
+            const res = {
+                status : 400,
+                errorCode : -102,
+                message : 'The bike is already in use!'
+            }; 
+            return ctx.send(res);
         }
-        
-        //Create usage fields
-        const userId = String(ctx.request.body.userId);
-        const bikeId = bike.id;
-        const startDockerId = String(ctx.request.body.dockerId);
-        const isOpen = true;
 
+        //Check the user has no bike currently
+        const userOpenUsages = await Usages.findOne({userId : ctx.request.body.userId , isOpen : true});
+        if(userOpenUsages){
+            const res = {
+                status : 400,
+                errorCode : -103,
+                message : 'User has already a bike!'
+            }; 
+            return ctx.send(res);
+        }
+
+        //Check the user balance
+        const user = await strapi.query('user','users-permissions').findOne({_id : ctx.request.body.userId});
+        //Check user
+        if(!user){
+            const res = {
+                status : 404,
+                errorCode : -104,
+                message : 'There is no such a user!'
+            };
+            return ctx.send(res);
+        }
+        //Check user has min 10 tl
+        if(user.balance < 10){
+            //Check user has a debt or not if balance == 0
+            if(!user.balance){
+                const userUsages = await Usages.find({userId : ctx.request.body.userId , isOpen : false}).sort({updatedAt : -1});
+                if(userUsages[0]){
+                    const inDebt = await Payments.findOne({usageId : userUsages[0]._id, inDebt : true});
+                    if(inDebt){
+                        const res = {
+                            status : 400,
+                            errorCode : -105,
+                            message : 'User has ' + (inDebt.totalPayment - inDebt.totalPaid) + ' tl debt!'
+                        }; 
+                        return ctx.send(res);
+                    }
+                }
+            }
+            const res = {
+                status : 400,
+                errorCode : -106,
+                message : 'User balance under 10 tl! '
+            };
+            return ctx.send(res);
+       }
+        
         //Create usage record
-        //TODO Check Catch
         try{
-            await strapi.query('usages').create({
-                userId:userId,
-                bikeId:bikeId,
-                startDockerId:startDockerId,
-                isOpen: isOpen
-            });
-        }catch(err){
-            ctx.send({message: err});
-        }
-        
-        //Update bike availability
-        //TODO Check Catch
-        try{
-            await strapi.query('bikes').update({barcode : ctx.request.body.qrCode},{$set: {isAvailable : false}}); 
-        }catch(err){
-            ctx.send({message: err});
-        }
+            //Create usage fields
+            const usage = {
+                userId : String(ctx.request.body.userId),
+                bikeId : bike._id,
+                startDockerId : String(ctx.request.body.dockerId),
+                isOpen : true
+            };
 
-        //Return Response
-        const status = 200;
-        const message = 'OK';
-        return ctx.response.send({ status : status, message : message });
-  
+            //Insert to Usages
+            await strapi.query('usages').create(usage);
+
+            //Update bike availability
+            await bike.updateOne({$set: {isAvailable : false}});
+
+            //Return Response
+            const res = {
+                status : 200,
+                data : usage,
+                message : 'OK'
+            }; 
+            return ctx.send(res);
+
+        }catch(err){
+            const res = {
+                status : 304,
+                errorCode : -100,
+                message : err
+            }; 
+            return ctx.send(res);
+        }
     },
     
     endSession: async ctx => {
+        
        
         //Check user has a bike currently
-        const currentUsage = await strapi.query('usages').findOne({userId : ctx.request.body.userId , isOpen : true});
+        const currentUsage = await Usages.findOne({userId : ctx.request.body.userId , isOpen : true});
         if(!currentUsage){
-            const status = 404;
-            const error = "End Session Error!";
-            const message = 'End Session could not be started due to no open session!';
-            return ctx.response.send({ status : status, error : error, message : message });
+            const res = {
+                status : 404,
+                errorCode : -111,
+                message : 'The user has no any open session!'
+            };
+            return ctx.send(res);
         }
-        
-        //Create usage fields
-        const endDockerId = String(ctx.request.body.dockerId);
 
-        //Update usage record
-        //TODO Check Catch
         try{
-            await strapi.query('usages').update({userId : ctx.request.body.userId , isOpen : true},{$set: {
-                isOpen : false,
-                endDockerId : endDockerId
-            }});  
+            //Update usage record
+            await currentUsage.updateOne({$set: {isOpen : false, endDockerId : String(ctx.request.body.dockerId)}}); 
+
+            //Update bike availability
+            await Bikes.updateOne({_id : currentUsage.bikeId},{$set: {isAvailable : true, lastDockerId : ctx.request.body.dockerId }});
+
+            //Find total payment
+            const finishedUsage = await Usages.findOne({_id : currentUsage._id});
+            const timeDifference = (finishedUsage.updatedAt - finishedUsage.createdAt) / (1000 * 60);
+
+            var totalPayment = 0.00;
+            if(timeDifference > 5) totalPayment += 5.00;
+            if(timeDifference > 60) totalPayment += (timeDifference-60)*0.1;
+            if(timeDifference > 1440) totalPayment += (timeDifference-1440)*0.4;
+
+            //Check user balance
+            const user = await strapi.query('user','users-permissions').findOne({_id : ctx.request.body.userId});
+            var newBalance = user.balance - totalPayment;
+            var totalPaid = totalPayment;
+            var inDebt = false;
+            if(newBalance < 0 ){
+                totalPaid = user.balance;
+                newBalance = 0;
+                inDebt = true;
+            }
+
+            //Update user balance
+            await strapi.query('user','users-permissions').update({_id : ctx.request.body.userId},{$set: {balance : newBalance}});
+
+            //Create payment fields
+            const payment = {
+                usageId : finishedUsage._id,
+                totalPayment : totalPayment,
+                totalPaid : totalPaid,
+                inDebt : inDebt
+            }
+
+            //Insert to Payments
+            await strapi.query('payments').create(payment);
+
+            //Return Response
+            const res = {
+                status : 200,
+                data : payment,
+                message : 'OK'
+            };  
+            return ctx.send(res);
+            
         }catch(err){
-            ctx.send({message: err});
+            const res = {
+                status : 304,
+                errorCode : -110,
+                message : err
+            }; 
+            return ctx.send(res);
         }
-
-        //Update bike availability
-        //TODO Check Catch
-        try{
-            await strapi.query('bikes').update({id : currentUsage.bikeId},{$set: {
-                isAvailable : true
-            }}); 
-        }catch(err){
-            ctx.send({message: err});
-        }
-
-        //Find total payment
-        const finishedUsage = await strapi.query('usages').findOne({id : currentUsage.id});
-        const timeDifference = (finishedUsage.updatedAt - finishedUsage.createdAt) / (1000 * 60);
-        var totalPayment = 0.00;
-        if(timeDifference > 5) totalPayment += 5.00;
-        if(timeDifference > 60) totalPayment += (timeDifference-60)*0.1;
-        if(timeDifference > 1440) totalPayment += (timeDifference-1440)*0.4;
-
-        //Create payment record
-        //TODO Check Catch
-        try{
-            await strapi.query('payments').create({
-                usageId:finishedUsage.id,
-                totalPayment:totalPayment,
-                isPaid: false
-            });
-        }catch(err){
-            ctx.send({message: err});
-        }
-
-        //Return Response
-        const status = 200;
-        const message = 'OK';
-        return ctx.response.send({ status : status, message : message });
     },
 
     openSession: async ctx => {
 
         //Find users open session
-        const currentUsage = await strapi.query('usages').findOne({userId : ctx.params.userId , isOpen : true});
+        const currentUsage = await Usages.findOne({userId : ctx.params.userId , isOpen : true});
         if(!currentUsage){
-            const status = 404;
-            const error = "Open Session Error!";
-            const message = 'No open session for user ' + ctx.params.userId + '!';
-            return ctx.response.send({ status : status, error : error, message : message });
+            const res = {
+                status : 404,
+                errorCode : -121,
+                message : 'The user has no any open session!'
+            };
+            return ctx.send(res);
         }
         //Return Response
-        const status = 200;
-        const message = 'OK';
-        return ctx.response.send({ status : status, data : currentUsage, message : message });
+        const res = {
+            status : 200,
+            data : currentUsage,
+            message : 'OK'
+        };  
+        return ctx.send(res);
     },
     
 };
