fda6f3a57eaa2be470a999933cd618c49180f794
aykutyrdm <aykutyrdm@gmail.com>
2020-03-03 20:19:22 +0300

    version_2.4.3

--- api/dockers/controllers/Dockers.js
+++ api/dockers/controllers/Dockers.js
@@ -3,11 +3,30 @@ var Turf = require('@turf/turf');
 
 module.exports = {
 
+    closestZone : async ctx => {
+
+        var location = JSON.parse(ctx.params.location);
+        location = [location.longitude, location.latidude];
+        var targetPoint = Turf.point(location);
+
+        var centers = [];
+        const dockers = await strapi.query('dockers').find();
+        for(var i = 0 ; i< dockers.length; i++){
+            var currentDocker = dockers[i];
+            const polygon  = Turf.polygon(currentDocker.coordinates.geometry.coordinates);
+            const center = Turf.centroid(polygon);
+            const currentAvails = await strapi.query('bikes').find({lastDockerId : currentDocker.id, isAvailable : true});
+            if(currentAvails.length) centers.push(Turf.point(center.geometry.coordinates));
+        }
+        centers = Turf.featureCollection(centers);
+        var nearest = Turf.nearestPoint(targetPoint,centers);
+        ctx.send(nearest);
+    },
+
     withBikes : async ctx => {
 
         //Find Dockers and it's bikes that are available currently
         const dockers = await strapi.query('dockers').find();
-        const dockerNumber = dockers.length;
         var dockersWithBikes = [];
         for(var i = 0 ; i< dockerNumber; i++){
             var currentDocker = dockers[i];
