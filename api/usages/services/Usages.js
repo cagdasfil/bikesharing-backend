2b02dbec47e040960c05ac52f8e38ad4cf6c7386
CAGDAS FIL <e2093839@ceng.metu.edu.tr>
2020-05-10 11:26:24 +0300

    findTotalFee updated

--- api/usages/services/Usages.js
+++ api/usages/services/Usages.js
@@ -8,11 +8,13 @@ module.exports = {
 
     findTotalFee : async function(timeDifference, totalPayment){
 
-        totalPayment = 0;
-        totalPayment = 15.00 + (timeDifference * 0.1);
-        //if(timeDifference > 5) totalPayment += 5.00;
-        //if(timeDifference > 60) totalPayment += (timeDifference-60)*0.1;
-        //if(timeDifference > 1440) totalPayment += (timeDifference-1440)*0.4;
+        if(timeDifference < 2){
+            totalPayment = 0
+        }
+        else{
+            totalPayment = 2.00;
+            totalPayment += timeDifference * 0.1;
+        }
         return totalPayment;
     },
     
